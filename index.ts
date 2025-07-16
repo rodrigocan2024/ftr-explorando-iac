eb5c813b3c3f63aa0897da634a5442781735d9af
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-16 18:13:34 -0300

    new: create new resource

--- index.ts
+++ index.ts
@@ -7,6 +7,13 @@ const firstBucket = new aws.s3.BucketV2("primeiro-bucket-teste2", {
   }
 });
 
+const secondBucket = new aws.s3.BucketV2("segundo-bucket-83974298", {
+  bucket: "segundo-bucket-83974298",
+  tags: {
+    IAC: "true",
+  }
+});
+
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
   name: "primeiro-ecr",
   imageTagMutability: "IMMUTABLE",
@@ -18,6 +25,10 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
