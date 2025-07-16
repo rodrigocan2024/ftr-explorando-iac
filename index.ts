bbc63c3849bc02650b03001c95b88c3dad1ae8d7
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-15 23:09:19 -0300

    Criando um repositório ECR

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("primeiro-teste-pos-rocketseat2", {
-  bucket: "primeiro-teste-pos-rocketseat2",
+const bucket = new aws.s3.BucketV2("primeiro-bucket-teste2", {
+  bucket: "primeiro-bucket-teste2",
   tags: {
     IAC: "true",
   }
 });
 
+const ecr = new aws.ecr.Repository("primeiro-ecr", {
+  name: "primeiro-ecr",
+  imageTagMutability: "IMMUTABLE",
+  tags: {
+    IAC: "true",
+  }
+})
+
 export const bucketName = bucket.id;
 export const bucketRegion = bucket.region;
 export const bucketArn = bucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
